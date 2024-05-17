bf9174230557ce4914c77e229a0ad9a44fba3d73
your <jaiswalanshikaajay.7@gmail.com>
2024-05-17 22:29:36 +0530

    bug fixes

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const ProjectCard = ({ link, src, title, description }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] z-[30] w-[33%]">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] z-[30] md:w-[33%] ">
      <a href={link}
      target="_blank"
      className="cursor-pointer">
